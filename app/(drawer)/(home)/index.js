0d65595e0a766a5bf3aa7daff47529d2a28d05e4
abdulrafay-1 <abdulrock413@gmail.com>
2024-06-03 17:42:45 +0500

    created homepage

--- app/(drawer)/(home)/index.js
+++ app/(drawer)/(home)/index.js
@@ -1,15 +1,77 @@
 import { Link } from 'expo-router';
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View, } from 'react-native'
+import taxi from "../../../assets/taxi.png"
+import fries from "../../../assets/frenchfries.png"
+import { AntDesign } from '@expo/vector-icons';
+
 
 const HomePage = () => {
   return (
-    <View>
-      <Text>HomePage</Text>
-      <Link href='(tabs)'>Order Ride</Link>
-      <Link href='(tabs)/food'>Order Food</Link>
+    <View style={styles.maincon}>
+      <Text style={styles.heading}>RIDE APP</Text>
+      <View style={styles.container}>
+        <Link href="(tabs)"><View style={styles.card}>
+          <Image source={taxi} style={styles.imageIcons} />
+          <View style={styles.cardTextCon}>
+            <Text style={styles.cardText}>Book Ride</Text>
+            <AntDesign name="arrowright" size={24} color="black" />
+          </View>
+        </View></Link>
+        <Link href='(tabs)/food'>
+          <View style={styles.card}>
+            <Image source={fries} style={styles.imageIcons} />
+            <View style={styles.cardTextCon}>
+              <Text style={styles.cardText}>Order Food</Text>
+              <AntDesign name="arrowright" size={24} color="black" />
+            </View>
+          </View>
+        </Link>
+      </View>
     </View>
   )
+
+
 }
+
+const styles = StyleSheet.create({
+  maincon: {
+    backgroundColor: "#ffffff",
+    flex: 1,
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    padding: 10,
+    color: "#555555"
+  },
+
+  container: {
+    display: 'flex',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+    gap: 10,
+  },
+  imageIcons: {
+    width: 120,
+    height: 120,
+    textAlign: 'center',
+  },
+  card: {
+    backgroundColor: "#e4e4e4",
+    padding: 12,
+    borderRadius: 20,
+  },
+  cardTextCon: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  cardText: {
+    fontSize: 20,
+    fontWeight: "500",
+  }
+});
 
 export default HomePage;
--- app/(drawer)/_layout.js
+++ app/(drawer)/_layout.js
@@ -1,26 +1,28 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 export default function DrawerLayout() {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <Drawer>
-                
                 <Drawer.Screen
                     name="(home)/index" // This is the name of the page and must match the url from root
                     options={{
                         drawerLabel: 'Home',
                         title: 'Home',
+                        drawerIcon: (color) => <Ionicons name="home-outline" size={24} color={color} />,
+
                     }}
                 />
                 <Drawer.Screen
                     name="(tabs)" // This is the name of the page and must match the url from root
                     options={{
                         drawerLabel: 'Ride',
-                        title: 'Ride',
+                        title: 'Ride & Food',
+                        drawerIcon: (color) => <MaterialIcons name="directions-bike" size={24} color={color} />,
                     }}
                 />
-                
             </Drawer>
         </GestureHandlerRootView>
     );
